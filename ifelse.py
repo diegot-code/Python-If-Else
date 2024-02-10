2aba50bcba589648cbbd2144c31eb5305d37f5f2
Diego Torres <126494563+diegot-code@users.noreply.github.com>
2024-02-10 18:53:17 -0500

    Added Username Eligibility

--- ifelse.py
+++ ifelse.py
@@ -1,6 +1,8 @@
 
 # Logic
 
+print("Start of Intro to Python Logic")
+
 x = 40
 
 y = 70.30
@@ -15,11 +17,27 @@ z = 17.00
 
 #----------------------------------------------
 
+# print(int == type(x))
+
+# print(type(x) == int)
+
+
+#----------------------------------------------
+
 # if (x < y):
-#     print(x, "is less than", y)
+#     print(x, "is less than", y) # good
 
+# if x < y: # the ()'s are not needed, but helpful
+#     print(x, "is less than", y) # good
 
-# if (x < y): print(x, "is less than", y)
+#----------------------------------------------
+
+# if (x < y): print(x, "is less than", y) #good
+
+# if (x > y): print(x, "is less than", y) elif(x==40):print("x is 40") # bad
+
+# if (x > y): print(x, "is less than", y)  else(x==40):print("x is 40") # bad
+
 
 #----------------------------------------------
 
@@ -50,26 +68,34 @@ start = "This variable just needs to be set."
 
 #----------------------------------------------
 
+highscore = 190500
 
-# if (type(x) == str):
-#     print("x is a string")
-# elif (type(x) == float):
-#     print("x is a float")
-# elif (type(x) == int): 
-#     print("x is a integer")
+# if (type(highscore) == str):
+#     print("highscore is a string")
+# elif (type(highscore) == float):
+#     print("highscore is a float")
+# elif (type(highscore) == int): 
+#     print("highscore is an integer")
 # else: 
-#     print("I don't know what x is.")
+#     print("I don't know what highscore is.")
 
 #______________________________________________________________________________________________________________________________________
 #----------------------------------------------
 
 username = "CoderFixerUpper12"
 
-if (type(username) == str):
-    if(username.isalpha()):
-        print("You need at least 1 number in your username to register:", username)
-    elif(username.isdigit()):
-        print("Your username needs at least 3 letters:", username)
-    # elif
+# if (type(username) == str):
+#     if(username.isspace()):  # check for whitespace
+#         print("You need a username containing numbers and letters", username)
+#     elif(username.isdigit()): # check if everything in the string is all numbers
+#         print("Your username needs at least 3 letters:", username)
+#     elif(username.isalpha()): # checks if everything is a letter
+#         print("You need at least 1 number in your username to register:", username)
+#     elif(username.isidentifier() == False):
+#         print("Your username is in the wrong syntax:", username)
+#     else:
+#         print("Your username", username, "is being registered...")
+# else:
+#     print("Your Username must be a string:", username)
 
 
