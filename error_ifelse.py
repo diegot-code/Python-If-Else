f999282d0ecce5e487d0ab7e141e295d025cb8b2
Diego Torres <126494563+diegot-code@users.noreply.github.com>
2024-02-20 22:44:22 -0500

    Common Errors in If/Else
    Made a File specially for errors in if/else.

---
+++ error_ifelse.py
@@ -0,0 +1,32 @@
+x = 23
+
+y = 40
+
+# Colons
+
+# if x > 5  # Missing colon
+#     print("x is greater than 5")
+
+
+# Indentation
+
+# if x > 5:
+# print("x is greater than 5")  # Incorrect indentation
+
+
+# if x > 5:
+#     print("x is greater than 5")
+# 	if y > 10:  # Mixing tabs and spaces
+# 		print("y is greater than 10")
+
+
+# if(x):
+# if(y):
+# print("x and y are assigned.")
+
+# Assignment Operator instead of Comparison Operator
+
+# if x = 5:  # Using = instead of ==
+#     print("x is equal to 5")
+
+# Indentation
